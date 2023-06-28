a4f98430a96e463328c8143e9b72123b9b12ac15
Briton Otieno <ojwangbriton@gmail.com>
2023-06-28 14:27:19 +0300

    Update index.html file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import './style.css';
 
-import { renderLeaderboard } from './modules/scoreList.js';
+import { renderLeaderboard, refreshScores, submitScore } from './modules/scoreList.js';
 
+// Your game ID
+const gameId = 'YOUR_GAME_ID';
+
+// Render the leaderboard initially
 renderLeaderboard();
+
+// Refresh scores when the "Refresh" button is clicked
+const refreshButton = document.querySelector('button[type="reset"]');
+refreshButton.addEventListener('click', () => {
+  refreshScores(gameId);
+});
+
+// Submit a score when the "Submit" button is clicked
+const submitButton = document.querySelector('button[type="submit"]');
+submitButton.addEventListener('click', async () => {
+  const nameInput = document.querySelector('input[name="name"]');
+  const scoreInput = document.querySelector('input[name="score"]');
+  const name = nameInput.value;
+  const score = parseInt(scoreInput.value, 10);
+
+  if (name && !Number.isNaN(score)) {
+    await submitScore(gameId, name, score);
+    await refreshScores(gameId);
+    nameInput.value = '';
+    scoreInput.value = '';
+  }
+});
