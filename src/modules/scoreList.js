2c271d42250d20c29f3178208352be26141cf6c0
Briton Otieno <ojwangbriton@gmail.com>
2023-06-28 14:29:28 +0300

    Update scoreList module

--- src/modules/scoreList.js
+++ src/modules/scoreList.js
@@ -1,10 +1,5 @@
 // Array of names and scores
-const leaderboard = [
-  { name: 'John', score: 100 },
-  { name: 'Alice', score: 85 },
-  { name: 'Bob', score: 95 },
-  { name: 'Jane', score: 90 },
-];
+let leaderboard = [];
 
 // Function to render the leaderboard
 export const renderLeaderboard = () => {
@@ -16,8 +11,34 @@ export const renderLeaderboard = () => {
   // Create HTML elements for each entry using template literals and arrow function
   leaderboard.forEach((entry, index) => {
     const listItem = document.createElement('li');
-    listItem.textContent = `${index + 1}. ${entry.name}: ${entry.score}`;
+    listItem.textContent = `${index + 1}. ${entry.user}: ${entry.score}`;
 
     leaderboardContainer.appendChild(listItem);
   });
+};
+
+// Function to refresh scores from the API
+export const refreshScores = async (gameId) => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`);
+  const data = await response.json();
+  leaderboard = data.result;
+
+  // Sort the leaderboard by score in descending order
+  leaderboard.sort((a, b) => b.score - a.score);
+
+  renderLeaderboard();
+};
+
+// Function to submit a score to the API
+export const submitScore = async (gameId, name, score) => {
+  await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user: name, score }),
+  });
+
+  // Add the new score to the leaderboard
+  leaderboard.push({ name, score });
 };
